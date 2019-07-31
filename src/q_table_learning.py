e86890433d0705b38ebcbd06be0205d2e3c0d730
Darren Lawton <darren@darrenlawton.com>
2019-08-01 09:59:59 +1000

    first commit.

---
+++ src/q_table_learning.py
@@ -0,0 +1,31 @@
+import numpy as np
+import gym
+
+env = gym.make('FrozenLake-v0')
+
+q_table = np.zeros([env.observation_space.n,env.action_space.n])
+learning_rate = 0.8
+discount_rate = 0.95
+n_episodes = 2000
+
+reward_list = []
+
+for i in range(n_episodes):
+    state = env.reset()
+    all_rewards = 0
+    end_state = False
+    batch = 0
+
+    while batch < 99:
+        batch += 1
+
+        # choose action greedily (with noise)
+        action = np.argmax(q_table[state,:] + np.random.randn(1, env.action_space.n)*(1./(i+1)))
+        # given action, get new state and reward
+        state_1, reward, end_state, _ = env.step(action)
+        # update q-table with new knowledge
+        
+
+
+
+
