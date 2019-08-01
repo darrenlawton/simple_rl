4e34a46ddc62c55b47fe30d689a7b70cfa7016ec
Darren Lawton <darren@darrenlawton.com>
2019-08-01 13:47:20 +1000

    q table done

--- src/q_table_learning.py
+++ src/q_table_learning.py
@@ -9,11 +9,10 @@ discount_rate = 0.95
 n_episodes = 2000
 
 reward_list = []
-
 for i in range(n_episodes):
     state = env.reset()
     all_rewards = 0
-    end_state = False
+    goal_state = False
     step = 0
 
     while step < 99:
@@ -21,10 +20,21 @@ for i in range(n_episodes):
         # choose action greedily (with noise)
         action = np.argmax(q_table[state,:] + np.random.randn(1, env.action_space.n)*(1./(i+1)))
         # given action, get new state and reward
-        state_1, reward, end_state, _ = env.step(action)
+        state_1, reward, goal_state, _ = env.step(action)
         # update q-table with new knowledge
-        q_table[state, action]
+        q_table[state, action] = q_table[state, action] + learning_rate * (reward + discount_rate *
+                                                                           np.max(q_table[state_1,:]) -
+                                                                           q_table[state, action])
 
+        all_rewards += reward
+        state = state_1
+
+        if goal_state:
+            break
+
+    reward_list.append(all_rewards)
+
+print("Q Values: {} " .format(q_table))
 
 
 
